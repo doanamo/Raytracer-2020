02e786bb05ffea0c0cae692e43f75e22b0c37e8d
Piotr Doan <doanpiotr@gmail.com>
2019-12-16 21:47:36 +0100

    Generate diffuse and normals examples using tests

--- tests/examples.rs
+++ tests/examples.rs
@@ -40,4 +40,68 @@ mod examples
 
         setup.save("examples/spheres.json").expect("Saving setup file failed!");
     }
+
+    #[test]
+    fn diffuse()
+    {
+        let parameters = render::Parameters
+        {
+            image_width: 1024,
+            image_height: 576,
+            antialias_samples: 4,
+            scatter_limit: 32,
+            debug_mode: Some(render::DebugMode::Diffuse),
+            ..render::Parameters::default()
+        };
+
+        let camera = render::Camera::new()
+            .set_origin(Vec3::new(0.0, -0.6, 0.0))
+            .set_look_at(Some(Vec3::new(0.0, 1.0, -0.2)))
+            .set_field_of_view(55.0);
+        
+        let scene = render::Scene::new()
+            .set_camera(camera)
+            .add_primitive(primitive::Sphere::new(Vec3::new(0.0, 0.5, -0.1), 0.4, material::Diffuse::from(Color::new(0.8, 0.8, 0.8, 1.0))))
+            .add_primitive(primitive::Sphere::new(Vec3::new(0.0, 1.0, -100.5), 100.0, material::Diffuse::from(Color::new(0.8, 0.8, 0.8, 1.0))));
+
+        let setup = setup::Setup
+        {
+            parameters,
+            scene
+        };
+
+        setup.save("examples/diffuse.json").expect("Saving setup file failed!");
+    }
+
+    #[test]
+    fn normals()
+    {
+        let parameters = render::Parameters
+        {
+            image_width: 1024,
+            image_height: 576,
+            antialias_samples: 4,
+            scatter_limit: 1,
+            debug_mode: Some(render::DebugMode::Normals),
+            ..render::Parameters::default()
+        };
+
+        let camera = render::Camera::new()
+            .set_origin(Vec3::new(0.0, -0.6, 0.0))
+            .set_look_at(Some(Vec3::new(0.0, 1.0, -0.2)))
+            .set_field_of_view(55.0);
+        
+        let scene = render::Scene::new()
+            .set_camera(camera)
+            .add_primitive(primitive::Sphere::new(Vec3::new(0.0, 0.5, -0.1), 0.4, material::Diffuse::from(Color::new(0.8, 0.8, 0.8, 1.0))))
+            .add_primitive(primitive::Sphere::new(Vec3::new(0.0, 1.0, -100.5), 100.0, material::Diffuse::from(Color::new(0.8, 0.8, 0.8, 1.0))));
+
+        let setup = setup::Setup
+        {
+            parameters,
+            scene
+        };
+
+        setup.save("examples/normals.json").expect("Saving setup file failed!");
+    }
 }
